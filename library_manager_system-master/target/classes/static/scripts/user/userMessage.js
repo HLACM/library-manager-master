6242207b2a880eebc2967dc5e46d6418f611f845
itcast <chl18307598501@qq.com>
2023-09-08 12:07:39 +0800

    redis2.0

--- library_manager_system-master/target/classes/static/scripts/user/userMessage.js
+++ library_manager_system-master/target/classes/static/scripts/user/userMessage.js
@@ -7,16 +7,18 @@ layui.use(['form', 'element','layer'], function () {
 $(document).ready(function () {
     // 点击修改信息按钮触发
     $('#modify-user').click(function () {
-        //页面层
+        //弹出层
         layer.open({
-            type: 1 //Page层类型
-            , skin: 'layui-layer-molv'
-            , area: ['380px', '270px']
-            , title: ['修改信息', 'font-size:18px']
-            , btn: ['取消', '保存']
-            , shade: 0 //遮罩透明度
-            , content: $("#window")
+            type: 1 //Page层类型，页面层
+            , skin: 'layui-layer-molv' //弹层皮肤样式
+            , area: ['380px', '270px'] //弹出层大小
+            , title: ['修改信息', 'font-size:18px'] //弹出层标题
+            , btn: ['保存', '取消'] //弹出层底部按钮
+            , shade: 0 //遮罩透明度。不显示遮罩层
+            , content: $("#window") //弹出层的内容
+            // yes参数，点击第一个按钮后执行方法
             , yes: function () {
+                // 执行更新用户信息
                 updateUser();
             }
         });
